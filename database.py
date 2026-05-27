705ce751d09237b24c4336cc07cfa17aac169160
HANAMAGOUDA <hanamagoudpatil9611@gmail.com>
2026-05-27 13:18:34 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ from psycopg2.extras import RealDictCursor, register_hstore
 
 def get_db():
     conn = psycopg2.connect(
-        dbname="nominatim_17_4m",
-        user="postgres",
-        password="imarg@2025",
-        host="10.10.6.252",
-        port="7051",
+        dbname="",
+        user="",
+        password="",
+        host="",
+        port="",
         cursor_factory=RealDictCursor
     )
     register_hstore(conn)
